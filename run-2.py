7428c662baca9a2e7ee82f2b8e503129cfa515fd
sincerefly <sincerefly@163.com>
2015-06-16 15:30:28 +0800

    Modify the encode to utf-8, change Chinese to English.

--- run-2.py
+++ run-2.py
@@ -10,39 +10,39 @@ import base64
 import urllib2
 
 # -----------------------------------------------------------------
-# 这个程序可以爬取http://data.eastmoney.com/report/ 网站的股评数据
+# This tools can get info from 'http://data.eastmoney.com/report'
 # -----------------------------------------------------------------
 
-# static
-MAX_NUMBER = 156
+# Static variable
+MAX_NUMBER = 158
 startTime = int(time.time())
 
 client = MongoClient('localhost', 27017)
 db = client.guping
 
-# 匹配网站的数据
+# re. used by webdriver
 pattern = re.compile(r'<tbody>([\s\S]+)</tbody>')
-# the detail page get author
-pattern2 = re.compile(r'<div class="report-infos">([\s\S]+)</div>')
 
+# Loop for get page list
 print "-"*6 + ' start ' + '-'*6
 for page in range(1, MAX_NUMBER + 1):
 
-    # 拼接网站的URL
+    # The Page list URL
     params = 'tp=1&cg=0&dt=2&page=' + str(page)
     base64encode = base64.b64encode(params)
 
     URL = 'http://data.eastmoney.com/report/#' + base64encode
+    print URL
 
     driver = webdriver.PhantomJS("./phantomjs")
     driver.set_window_size(1366, 768) # optional
     driver.get(URL)
 
     bodyStr= driver.find_element_by_tag_name("body").get_attribute("innerHTML")
-    #print bodyStr
 
-    driver.quit() # 每一次抓取URL都需要关闭driver，重新建立，否则无法循环使用
+    driver.quit() # Every time you need to quit the driver 
 
+    # match the useful data
     match = pattern.search(bodyStr)
 
     if match:
@@ -50,46 +50,47 @@ for page in range(1, MAX_NUMBER + 1):
     else:
         print 'Error: ' + str(page) + '/' + str(MAX_NUMBER) + 'No data'
 
-    #print string 
-    # 将数据每页数据拼接,保存到mongodb数据库中
+    # insert into mongodb per page 
     l = []
+    count = 0
     for row in BeautifulSoup(string)("tr"):
 
-	# ------ Get the detail page to get the author ------
-	#print row("td")[5]("a")[0]["href"]
+        count = count + 1
+        # ------ Get the detail page to get the author ------
+        lasturl = row("td")[5]("a")[0]["href"]
+        detail_url = 'http://data.eastmoney.com' + lasturl
+        #detail_url = 'http://data.eastmoney.com/report/20150615/APPGN1vwyH6mASearchReport.html'
+        print '\t -> ' + str(count) + '. '+ lasturl
 
-	detail_url = 'http://data.eastmoney.com' + row("td")[5]("a")[0]["href"]
-	#detail_url = 'http://data.eastmoney.com/report/20150615/APPGN1vwyH6mASearchReport.html'
-	#print detail_url
+        detail_page = urllib2.urlopen(detail_url)
+        soup = BeautifulSoup(detail_page)
 
-	detail_page = urllib2.urlopen(detail_url)
-	soup = BeautifulSoup(detail_page)
-
-	#print soup.find_all('div', class_='report-infos')[0]("span")[3].text
-	author2 = soup.find_all('div', class_='report-infos')[0]("span")[3].text
-	author2 = author2.split(',')
-
+        #print soup.find_all('div', class_='report-infos')[0]("span")[3].text
+        author2 = soup.find_all('div', class_='report-infos')[0]("span")[3].text
+        # the author2 is unicode 
+        #print isinstance(author2, unicode)
+   
+        author2_list = author2.split(',')
+        author2_utf8 = map(lambda x: x.encode('utf-8'), author2_list)
         # ---------------------------------------------------
 
-
         data = {
-            "date": row("td")[1]("span")[0]["title"],
-            "code": row("td")[2].text,
-            "name": row("td")[3].text,
-            "title": row("td")[5].text,
-            "suggest": row("td")[6].text,
-            "change": row("td")[7].text,
-            "author": row("td")[8].text,
-            "author2": author2,
-            "a1": row("td")[9].text,
-            "a2": row("td")[10].text,
-            "b1": row("td")[11].text,
-            "b2": row("td")[12].text,
+            "date": row("td")[1]("span")[0]["title"].encode('utf-8'),
+            "code": row("td")[2].text.encode('utf-8'),
+            "name": row("td")[3].text.encode('utf-8'),
+            "title": row("td")[5].text.encode('utf-8'),
+            "suggest": row("td")[6].text.encode('utf-8'),
+            "change": row("td")[7].text.encode('utf-8'),
+            "author": row("td")[8].text.encode('utf-8'),
+            "author2": author2_utf8,
+            "a1": row("td")[9].text.encode('utf-8'),
+            "a2": row("td")[10].text.encode('utf-8'),
+            "b1": row("td")[11].text.encode('utf-8'),
+            "b2": row("td")[12].text.encode('utf-8'),
         }
-        #print data
         l.append(data)
 
-    db.dfcf_up2.insert(l)
+    db.dfcf_up3.insert(l)
     useSeconds = str(int(time.time()) - startTime)
     print 'Success: ' + str(page) + '/' + str(MAX_NUMBER) + '\t' + base64encode +  '\tTotalUseTime: ' + useSeconds + 's'
 
